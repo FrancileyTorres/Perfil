bdee405d4a1fa2a38872f3de34efd76152c5337b
FrancileyTorres <114429605+FrancileyTorres@users.noreply.github.com>
2025-05-19 21:35:37 -0300

    Subir os arquivo iniciais do meu novo site 2025

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init({ duration: 1000 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
